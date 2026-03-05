cbd1a539751586178f9de0215042e00908a7771e
fabri <fabritacchi@gmail.com>
2026-03-05 16:09:19 -0300

    Ejercicio 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,28 +1,38 @@
 import { useState } from 'react'
 
-const Statistics = (props) => {
-  if (props.all == 0) {
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
+
+  if (all == 0) {
     return(
       <p>No feedback given</p>
     )
   }
 
   return(
-    <div>
-      <h1>statistics</h1>
-      <StatisticLine text={"good"} value={props.good}/>
-      <StatisticLine text={"neutral"} value={props.neutral}/>
-      <StatisticLine text={"bad"} value={props.bad}/>
-      <StatisticLine text={"all"} value={props.all}/>
-      <StatisticLine text={"average"} value={props.average}/>
-      <StatisticLine text={"positive"} value={props.positive}/>
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text={"good"} value={good}/>
+        <StatisticLine text={"neutral"} value={neutral}/>
+        <StatisticLine text={"bad"} value={bad}/>
+        <StatisticLine text={"all"} value={all}/>
+        <StatisticLine text={"average"} value={average}/>
+        <StatisticLine text={"positive"} value={positive}/>
+      </tbody>
+    </table>
   )
 }
 
 const StatisticLine = (props) => {
   return(
-    <div>{props.text} {props.value}</div>
+    <>
+      <tr>
+        <td>{props.text}</td>
+        <td>{props.value}</td>
+      </tr>
+    </>
   )
 }
 
@@ -43,13 +53,15 @@ const App = () => {
   const positive = (good / all) * 100
 
   return (
-    <div>
+    <>
       <h1>give feedback</h1>
       <Button onClick={() => setGood(good + 1)} text={"good"} />
       <Button onClick={() => setNeutral(neutral + 1)} text={"neutral"} />
       <Button onClick={() => setBad(bad + 1)} text={"bad"} />
-      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
-   </div>
+
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+   </>
   )
 }
 
